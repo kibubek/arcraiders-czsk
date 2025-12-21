f9e06ff4c98b6aba49e1dd14e738c7fca9623bfe
Michal Štefan <kibubek@gmail.com>
2025-12-21 03:43:49 +0100

    boost command test

--- src/commands/boost.js
+++ src/commands/boost.js
@@ -1,6 +1,23 @@
 ﻿module.exports = {
   name: "boost",
   description: "Ukaze vyhody vylepseni",
-  title: "Vyhody boostu",
-  body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris non tortor et elit tincidunt feugiat.",
+  title: "",
+  body: `# VÝHODY ZA VYLEPŠENÍ <:BoostPepe:1440033954555101267>
+
+ **<:boost:1447990884074520668>  Externí emoji z jiných serverů**
+Máš nitro a chceš ho využít naplno? S booster rolí můžeš na serveru používat jakýkoliv emote ze všech serverů na světě, včetně těch animovaných!
+
+** <:boost:1447990884074520668>  Návrh na 2 nové emoji podle vlastní volby**
+Emotikony serveru jsou sice dobré ale možná máš nápad na svůj? Nevadí, dostaneš přístup do kanálu kde nám můžeš poslat emoji který by jsi rád přidal.
+
+** <:boost:1447990884074520668>  Exkluzivní role a respekt**
+Chceš být viděn jako někdo kdo drží pilíř komunity (nebo má až moc peněz)? S rolí boostera to máš jednoduše zaručeno.
+
+** <:boost:1447990884074520668> Možnost vytvářet průzkumy kdekoliv **
+Zajímá tě něco nebo si chceš udělat hlasování jen tak ze srandy? o čemkoliv? Ano, jako booster můžeš vytvořit svoje menší hlasování kde chceš.
+
+** <:boost:1447990884074520668> Žádné omezení na zprávy**
+Někdy chceš posílat zprávy trochu rychleji, chápeme, ale nemůžeme to dovolit každému, takže pokud jsi booster tak na tebe omezení v <#1368360686790381610> neplatí.
+
+Po vylepšení serveru ihned obdržíte roli <@&1373095106684653669>`,
 };
